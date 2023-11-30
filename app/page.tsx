04e1ec1afff3c13c1608499891e83f3d799d6826
Dan Varela <danmar.dev.ph@tripleonetech.net>
2023-11-30 20:56:16 +0800

    add theme toggler element

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import GradientText from "@/components/gradient-text";
 import NavLink from "@/components/home/nav-link";
+import ThemeSwitcher from "@/components/theme-switcher";
 import { cn } from "@/lib/utils";
 import { Linkedin, LucideProps, Twitter, Mail } from "lucide-react";
 
@@ -13,7 +14,7 @@ export default function Home() {
       <div
         className={cn(
           "fixed inset-0 w-1/2 dark:bg-rx-slatedark1 bg-rx-slatelight1",
-          "flex justify-end",
+          "flex justify-end z-50",
         )}
       >
         <div className="w-full max-w-lg h-full pt-24 pb-12 flex flex-col justify-between">
@@ -54,6 +55,10 @@ export default function Home() {
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="fixed right-8 top-8">
+        <ThemeSwitcher />
       </div>
     </>
   );
--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -2,18 +2,28 @@
 
 import { useTheme } from "next-themes";
 import { useIsClient } from "usehooks-ts";
+import wandDark from "@/assets/wand-dark.png";
+import wandLight from "@/assets/wand-light.png";
+import { Moon, Sun } from "lucide-react";
 
 export default function ThemeSwitcher() {
   const { setTheme, resolvedTheme } = useTheme();
   const isClient = useIsClient();
 
+  const cursor = resolvedTheme === "light" ? wandDark : wandLight;
+
   if (!isClient) return null;
 
   return (
     <button
+      className="transition px-4 py-2 rounded-full hover:bg-rx-slatelight4 dark:hover:bg-rx-slatedark4 flex gap-2"
       onClick={() => setTheme(resolvedTheme === "light" ? "dark" : "light")}
+      style={{
+        cursor: `url(${cursor.src}), pointer`,
+      }}
     >
-      {resolvedTheme === "light" ? "Toggle Dark Theme" : "Toggle Light Theme"}
+      {resolvedTheme === "light" ? <Moon /> : <Sun />}
+      {resolvedTheme === "light" ? "Nox!" : "Lumos!"}
     </button>
   );
 }
