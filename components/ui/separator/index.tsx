960f48898398f7ceb750066b6caf14ac2fcd8e7b
Dan Varela <danmar.dev.ph@tripleonetech.net>
2023-12-11 17:57:41 +0800

    feat(separator): add separator components

---
+++ components/ui/separator/index.tsx
@@ -0,0 +1,26 @@
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
+import { Separator as RxSeparator } from "@radix-ui/react-separator";
+import { cn } from "@/lib/utils";
+
+export type SeparatorProps = ComponentPropsWithoutRef<typeof RxSeparator>;
+
+const Separator = forwardRef<ElementRef<typeof RxSeparator>, SeparatorProps>(
+  ({ className, orientation, ...props }, ref) => {
+    return (
+      <RxSeparator
+        className={cn(
+          "bg-rx-slatelight7 dark:bg-rx-slatedark7",
+          orientation === "horizontal" && "h-px w-full",
+          orientation === "vertical" && "w-px h-full",
+          className,
+        )}
+        {...props}
+        ref={ref}
+      />
+    );
+  },
+);
+
+Separator.displayName = "Separator";
+
+export { Separator };
