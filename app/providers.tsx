28647e8b282545713d72652ddfee44f20c472586
Dan Varela <danmar.dev.ph@tripleonetech.net>
2023-12-16 02:31:34 +0800

    feat(dark-theme): force dark theme

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,5 +4,9 @@ import { PropsWithChildren } from "react";
 import { ThemeProvider } from "next-themes";
 
 export default function Providers({ children }: PropsWithChildren) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class" forcedTheme="dark">
+      {children}
+    </ThemeProvider>
+  );
 }
