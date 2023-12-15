9fa4c66760165d93e48fe48356e3a65bd8b01082
Dan Varela <danmar.dev.ph@tripleonetech.net>
2023-12-16 07:24:10 +0800

    feat(home): add contact section and footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,14 @@ import { SliceZone } from "@prismicio/react";
 import { components } from "@/slices";
 import { createClient } from "@/prismicio";
 import { pick, omit } from "lodash";
+import Section from "@/components/layout/section";
+import {
+  SiGmail,
+  SiLinkedin,
+  SiTelegram,
+} from "@icons-pack/react-simple-icons";
+import { NavLink } from "@/components/nav-link";
+import { Separator } from "@/components/ui/separator";
 
 export default async function HomePage() {
   const client = createClient();
@@ -26,6 +34,38 @@ export default async function HomePage() {
           )}
           components={omit(components, "introduction")}
         />
+
+        <Section id="contact-me" className="mb-8">
+          <h1>Let's keep in touch</h1>
+          <div className="flex gap-8 flex-wrap not-prose font-gochi text-xl">
+            <NavLink
+              text="LinkedIn"
+              href="https://www.linkedin.com/in/danmar-varela-8097a9242/"
+              icon={<SiLinkedin className="w-4 h-4 mb-1" />}
+              target="_blank"
+            />
+            <NavLink
+              text="Email"
+              href="mailto:vareladanmarj@gmail.com"
+              icon={<SiGmail className="w-4 h-4 mb-1" />}
+              target="_blank"
+            />
+            <NavLink
+              text="Telegram"
+              href="https://telegram.me/danwtfizdiz"
+              icon={<SiTelegram className="w-4 h-4 mb-1" />}
+              target="_blank"
+            />
+          </div>
+        </Section>
+
+        <footer className="max-w-2xl h-full mx-auto p-4 prose prose-p:text-sm">
+          <Separator orientation="horizontal" className="mb-4" />
+          <p>
+            Made with Next.js, Typescript, TailwindCSS, and Radix UI. Content
+            managed by Prismic.io
+          </p>
+        </footer>
       </div>
     </div>
   );
